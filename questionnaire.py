911c8c11bfd993e68421071b3fb76e30b397bfd3
Agraj <148015852+jarga3@users.noreply.github.com>
2024-10-01 18:49:19 -0600

    Create questionnaire.py

---
+++ questionnaire.py
@@ -0,0 +1,52 @@
+questions = [
+    {
+        "question": "What type of student are you? ",
+        "choices": ["First Year", "Second Year", "Upper Year", "Graduate"],
+        "importance_level": 3
+    },
+    {
+        "question": "What is your age? ",
+        "choices": ["<22", "22-27", "28+"],
+        "importance_level": 3
+    },
+    {
+        "question": "What is your budget for an 8 month term? ",
+        "choices": ["Low (<$7000)", "Medium ($8000-9000)", "High (>$9000+)"],
+        "importance_level": 3
+    },
+    {
+        "question": "Are you okay with having a roommate? ",
+        "choices": ["Yes", "Yes, if my bedroom is private", "No"],
+        "importance_level": 2
+    },
+    {
+        "question": "What kind of bathroom would you like? ",
+        "choices": ["Communal", "Shared", "Private"],
+        "importance_level": 2
+    },
+    {
+        "question": "Are you planning to purchase a meal plan? ",
+        "choices": ["Yes", "No"],
+        "importance_level": 2
+    },
+    {
+        "question": "What kind of kitchen do you need? ",
+        "choices": ["Full-size Kitchen", "Kitchenette", "No kitchen"],
+        "importance_level": 2
+    },
+    {
+        "question": "What size of bed would you like if possible? ",
+        "choices": ["Twin XL", "Double XL", "Queen", "King"],
+        "importance_level": 1
+    },
+    {
+        "question": "Do you want to live directly on campus? ",
+        "choices": ["Yes", "No"],
+        "importance_level": 1
+    },
+    {
+        "question": "Are you part of the Scholar's Advantage program? ",
+        "choices": ["Yes", "No"],
+        "importance_level": 1
+    }
+]
